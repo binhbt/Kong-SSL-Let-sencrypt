5e74b9ff57c786c2360085a0a228d86382d089c1
BinhBT <binhbt@sigma-solutions.eu>
2020-01-14 17:00:26 +0700

    init src

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=kong.sigma-solutions.vn
+domains=(kong.sigma-solutions.vn)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="sigmasolutions.eu@gmail.com" # Adding a valid address is strongly recommended
